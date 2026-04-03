39b17b42b89c1168d0572600f1ffd6e7e2dd1c67
tmiguel78 <tmiguel78@yahoo.es>
2026-04-03 12:48:32 +0200

    diseño responsive mejorado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,11 +9,10 @@ const Home = () => {
         <main>
             <h2>· Quiénes somos:</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </main>
-        <article>
             <h2>· Nuestra metodología:</h2>
             <p>Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris. Integer in mauris eu nibh euismod gravida. Aliquam sit amet.</p>
-        </article>
+        </main>
+        
     </div>
     </>
     )
